2f45573967fe6497a94bb1dd3cf2ff67e29c69c5
NDragneelL9 <nell.dragneel@gmail.com>
2022-01-06 08:24:16 +0300

    Added create and destroy actions to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Something wrong with provided credentials'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully logged out'
+    redirect_to signin_path
+  end
+
+  private
+
+  def session_params; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
